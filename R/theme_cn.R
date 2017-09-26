84f495c391e7ca8e29c82217da519c35fa37eb33
zfb <zengfengbo@rrgenetech.com>
2017-09-26 22:12:59 +0800

    init

---
+++ R/theme_cn.R
@@ -0,0 +1,25 @@
+#' cn font for ggplot2
+#' @importFrom ggplot2 theme element_text
+#' @export
+#' @examples
+#' library(ggformula)
+#' gf_point(mpg ~ hp, color = ~ cyl, size = ~wt, data = mtcars) -> p
+#' p
+#' p + xlab("X轴") + ylab("Y轴") + ggtitle("标题") # + font_cn()
+#' p + xlab("X轴") + ylab("Y轴") + ggtitle("标题") + font_cn()
+font_cn <- function(font="STKaiti")
+{
+  # 1. 识别mac, linux, win.
+  # 2. 查找三个系统上的中文字体.
+  # 3. 随机选取一种字体
+  theme(
+    title = element_text(family = font),
+    axis.title.x = element_text(family = font),
+    axis.title.y = element_text(family = font)
+  )
+}
+
+#' cn for plot
+#' @examples
+#' plot(1:10, xlab="X轴", ylab="Y轴", main="标题")
+NULL
